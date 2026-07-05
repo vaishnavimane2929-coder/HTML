ecad66943d40ca27ff0374055ea843909d99c6dc
Vaishnavi Mane <vaishnavimane2929@gmail.com>
2026-07-05 22:23:14 +0530

    ParaMethod.java

---
+++ Java/ParaMethod.java
@@ -0,0 +1,18 @@
+class ParaMethod{
+	public static void main(String [] args){
+		m1(7,"hello",'c');
+		//String res1=m2('B',"sunday");
+		//System.out.println(res1);
+	}
+
+	public static void m1(int a,String b,char c){
+		System.out.println("int value:"+a);
+		System.out.println("String value:"+b);
+		System.out.println("char value:"+c);
+		
+	}
+	public static String m2(char b,String c){
+			String res=b+c;
+			return res;
+	}
+}
